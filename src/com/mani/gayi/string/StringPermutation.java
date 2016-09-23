6027f806713f98efb02828b60b5dc4e380436b77
manidhar <manidhar.guduri@gmail.com>
2016-09-24 00:09:13 +0530

    Adding the comments for the program...

--- src/com/mani/gayi/string/StringPermutation.java
+++ src/com/mani/gayi/string/StringPermutation.java
@@ -1,31 +1,39 @@
 package com.mani.gayi.string;
 
 public class StringPermutation {
-	
-	public static void main (String[] args) {
-	String str = "BOY";
-        int n = str.length();
-        permute(str.toCharArray(), 0, n-1);
+
+	/**
+	 * Permute function takes 3 arguments as input and returns nothing. This
+	 * function prints every possible permutation by using huge recursion. ;'
+	 */
+
+	public static void permute(char[] str, int start, int end) {
+		if (start == end) {
+			System.out.println(str);
+		}
+
+		for (int i = start; i <= end; i++) {
+			swap(str, start, i);
+			permute(str, start + 1, end);
+			swap(str, start, i);
+		}
 	}
-	
-	public static void permute(char[] str, int start, int end){
-	    if(start==end){
-	        System.out.println(str);        
-	    }
-	    
-	    for(int i=start;i<=end;i++){
-	        swap(str,start, i);
-	        permute(str, start+1, end);
-	        swap(str, start, i);
-	    }
+
+	public static void swap(char[] str, int i, int j) {
+		char tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
 	}
-	
-	public static void swap(char[] str, int i, int j){
-	    char tmp = str[i];
-	    str[i] = str[j];
-	    str[j] = tmp;
+
+	// This is driver method which test the program.
+	public static void main(String[] args) {
+		// creating the input string.
+		String str = "BOY";
+		// finding the length of the input string.
+		int n = str.length();
+		// calling the funciton by passing the 3 arguments i.e. String ,zero and
+		// string length -1.
+		permute(str.toCharArray(), 0, n - 1);
 	}
 
 }
-
-
