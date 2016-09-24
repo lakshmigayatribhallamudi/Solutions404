fc8dbea93a39f2a4b814efd621cd3e871533a899
mguduri <mguduri@manh.com>
2016-09-24 15:42:12 +0530

    All Sub Sequences Permutations

---
+++ src/com/mani/gayi/string/StringPermutationAllSubSequences.java
@@ -0,0 +1,41 @@
+package com.mani.gayi.string;
+
+public class StringPermutationAllSubSequences {
+
+
+	/**
+	 * Permute function takes 3 arguments as input and returns nothing. This
+	 * function prints every possible permutation by using huge recursion. ;'
+	 */
+
+	public static void permute(char[] str, int start, int end) {
+		for(int index = 0;index<start;index++){
+			System.out.print(str[index]);
+		}
+		System.out.println();
+		for (int i = start; i <= end; i++) {
+			swap(str, start, i);
+			permute(str, start + 1, end);
+			swap(str, start, i);
+		}
+	}
+
+	public static void swap(char[] str, int i, int j) {
+		char tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+	}
+
+	// This is driver method which test the program.
+	public static void main(String[] args) {
+		// creating the input string.
+		String str = "BOY";
+		// finding the length of the input string.
+		int n = str.length();
+		// calling the funciton by passing the 3 arguments i.e. String ,zero and
+		// string length -1.
+		permute(str.toCharArray(), 0, n - 1);
+	}
+
+	
+}
