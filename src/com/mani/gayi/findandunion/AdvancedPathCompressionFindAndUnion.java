7f015800ecebbd2cdef9e9f6168cccc9d8ec40a6
manidhar <manidhar.guduri@gmail.com>
2017-02-02 18:37:33 +0530

    Advanced Find and Union Algorithm.

---
+++ src/com/mani/gayi/findandunion/AdvancedPathCompressionFindAndUnion.java
@@ -0,0 +1,101 @@
+package com.mani.gayi.findandunion;
+
+import com.mani.gayi.utils.ArrayUtils;
+
+public class AdvancedPathCompressionFindAndUnion {
+
+	public static void main(String[] args) {
+
+		int arr[] = initializeArray();
+		int size[] = initializeSizeWithOne(arr.length);
+		
+		System.out.print("Old Array : ");
+		ArrayUtils.printIntegerArray(arr);
+		advancedUnionV2(arr, 1, 0,size);
+		System.out.println();
+		System.out.print("New Array :union(arr,1,0) ");
+		ArrayUtils.printIntegerArray(arr);
+		
+		advancedUnionV2(arr, 0, 2,size);
+		System.out.println();
+		System.out.print("New Array :union(arr,0,2) ");
+		ArrayUtils.printIntegerArray(arr);
+
+		advancedUnionV2(arr, 3, 4,size);
+		System.out.println();
+		System.out.print("New Array :union(arr,3,4) ");
+		ArrayUtils.printIntegerArray(arr);
+		
+		advancedUnionV2(arr, 1, 4,size);
+		System.out.println();
+		System.out.print("New Array :union(arr,1,4) ");
+		ArrayUtils.printIntegerArray(arr);
+		
+		System.out.println();
+		System.out.println(" 2 and 5 are Connected ? :" + isConnected(arr, 2, 5));
+
+		System.out.println();
+		System.out.println(" 1 and 5 are Connected ? :" + isConnected(arr, 1, 5));
+
+		System.out.println();
+		System.out.println(" 8 and 5 are Connected ? :" + isConnected(arr, 3,4));
+	}
+
+	private static boolean isConnected(int[] arr, int Aelement, int Belement) {
+		if(root(arr,Aelement)==root(arr,Belement))
+		return true;
+		else
+		return false;
+	}
+
+	
+	// we are joining the elements based on the weight.
+	private static void advancedUnionV2(int[] arr, int Aelement, int Belement,int[] size) {
+		
+		int arootelement = root(arr,Aelement);
+		int brootelement = root(arr,Belement);
+		if(size[arootelement]>=size[brootelement])
+		{
+			arr[brootelement]=arootelement;
+			size[arootelement]=size[brootelement]+1;
+		}
+		else
+		{
+			arr[arootelement]=brootelement;
+			size[brootelement]=size[arootelement]+1;
+		}
+		
+	}
+
+	public static int[] initializeArray() {
+		int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		return arr;
+	}
+	
+	public static int[] initializeSizeWithOne(int len) {
+		
+		int[] arr = new int[len];
+		for(int index=0;index<arr.length;index++)
+		{
+			arr[index]=1;
+		}
+		
+		return arr;
+	}
+	
+	
+	// Advanced root.
+	public static int root(int[] arr,int data)
+	{
+		while(arr[data]!=data)
+		{
+			arr[data]=arr[arr[data]];
+			data = arr[data];
+		}
+		return data;
+	}
+	
+	
+	
+
+}
