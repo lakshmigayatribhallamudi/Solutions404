6cb07439c269b8cc956c074b8d9d5366a4e47366
MGuduri <manidhar.guduri@gmail.com>
2017-02-27 23:55:51 +0530

    Inorder traversal without reccursion.

--- src/com/mani/gayi/tree/BinaryTreeTraversal.java
+++ src/com/mani/gayi/tree/BinaryTreeTraversal.java
@@ -7,7 +7,7 @@ public class BinaryTreeTraversal {
 	
 	/**
 	 * This function traversals thought the root and prints the following order:
-	 * Left -- Root -- Rights
+	 * Left -- Root -- Right
 	 * */
 	public static void inOrderUsingRecursion(BinaryTreeNode root)
 	{
@@ -28,6 +28,59 @@ public class BinaryTreeTraversal {
 	}
 	
 	
-
+	/**
+	 * This function traversals thought the root and print the following orders
+	 * Left -- Root -- Right in a Iterative Order.
+	 * */
+	public static void inOrderUsingIteration(BinaryTreeNode root)
+	{
+		// if the given tree node is null...
+		if(root == null)
+		{
+			return;
+		}
+		
+		// Given a Stack...
+		Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
+		
+		// Iterate till the root's left node is null...
+		while(root!=null)
+		{
+			//push the root in to the stack.
+			stack.push(root);
+			
+			// iterative to the left of the root.
+			root=root.getLeftNode();
+		}
+		
+		// iterate till the stack is empty.
+		while(stack.size()>0)
+		{
+			// now after this loop stack contains left most element...
+			BinaryTreeNode element = stack.pop();
+			
+			System.out.println("Stack Size : "+stack.size()+":"+element.getData()+":");
+			
+			// get the right node...
+			// check for the right node
+			if(element.getRightNode()!=null)
+			{
+				// if the right node exists.
+				element = element.getRightNode();
+				
+				// if the element is not null
+				while(element!=null)
+				{
+					stack.push(element);
+					element = element.getLeftNode();
+				}
+			}
+			
+			
+			
+		}
+		
+		
+	}
 	
 }
