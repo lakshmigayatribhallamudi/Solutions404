d2dd65e419eb5dd091ba521f0756bfd4769a4940
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2017-02-27 23:03:16 +0530

    Declaration of REnt Utility class

---
+++ src/com/mani/gayi/pg/app/RentUtility.java
@@ -0,0 +1,22 @@
+package com.mani.gayi.pg.app;
+
+/***
+ * 
+ * @author lbhallamudi
+ *
+ */
+public class RentUtility {
+	
+	private static final  String odd_Date="31";
+	private static final  String even_date="28";
+	private static final String feb_date="29";
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
