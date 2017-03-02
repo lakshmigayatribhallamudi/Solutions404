1078518b5af8051bf654d66b24d7d63a7f0d59d8
MGuduri <manidhar.guduri@gmail.com>
2017-03-02 22:45:01 +0530

    Formatting...

--- src/com/mani/gayi/pg/app/CollectionUtility.java
+++ src/com/mani/gayi/pg/app/CollectionUtility.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionUtility<T> {
-	
-	public List<T> addObjectToList(T obj)
-	{
-		List<T> l=new ArrayList<T>();
+
+	public List<T> addObjectToList(T obj) {
+		List<T> l = new ArrayList<T>();
 		l.add((T) obj);
 		return l;
-	} }
+	}
+}
