826076f7502c333235cf1d7a923d8a3bf4b5b7d4
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2017-03-07 23:37:52 +0530

    committing the fix files

--- src/com/mani/gayi/pg/app/FlatAPIImpl.java
+++ src/com/mani/gayi/pg/app/FlatAPIImpl.java
@@ -5,43 +5,53 @@ import java.util.List;
 
 public class FlatAPIImpl implements IFlatAPI {
 
-	
 	/*
 	 * This method creates a room and adds it to the given flat.
-	 * */
+	 */
 	@Override
 	public boolean addRoomToAFlat(Room room, Flat flat) {
-		
+
 		// if any object is null then we cannot add
-		if(room==null || flat ==null)
-		{
+		if (room == null || flat == null) {
 			return false;
 		}
-		
+
 		// if the flat contains rooms.
-		if(flat.getRooms()!=null)
-		{
+		if (flat.getRooms() != null) {
 			flat.getRooms().add(room);
-		}
-		else
-		{
+		} else {
 			// Fat doesn't contain any rooms...
 			flat.setRooms(new CollectionUtility<Room>().addObjectToList(room));
 		}
-		
+
 		// setting the flat object to the room.
 		room.setFlat(flat);
 		return true;
 	}
 
-	@Override
-	public void addFlatToaFloor(Flat flat, Floor floor) {
-		// TODO Auto-generated method stub
-		flat.setFloor(floor);
-		List<Flat> l=new ArrayList<Flat >();
-		l.add(flat);
-		floor.setFlats(l);
-		
-	}
+	/*
+	 * This method creates a flat and adds it to the given Floor.
+	 */
 
+	@Override
+	public boolean addFlatToaFloor(Flat flat, Floor floor) {
+		// TODO Auto-generated method stub
+
+		// if any object is null then we cannot add
+		if (flat == null || floor == null) {
+			return false;
+		}
+		// if floor contains flats.
+
+		if (floor.getFlats() != null) {
+			floor.getFlats().add(flat);
+		} else {
+			// floor doesnot contain any flats.
+
+			floor.setFlats(new CollectionUtility<Flat>().addObjectToList(flat));
+		}
+
+		flat.setFloor(floor);
+		return true;
+	}
 }
