10f776006946ee4d35842304c3ca6f04ccaea7c2
MGuduri <manidhar.guduri@gmail.com>
2017-03-07 23:17:43 +0530

    FlatAPI addRoomToAFlat method implmentation.

--- src/com/mani/gayi/pg/app/FlatAPIImpl.java
+++ src/com/mani/gayi/pg/app/FlatAPIImpl.java
@@ -5,12 +5,33 @@ import java.util.List;
 
 public class FlatAPIImpl implements IFlatAPI {
 
+	
+	/*
+	 * This method creates a room and adds it to the given flat.
+	 * */
 	@Override
-	public void addRoomsToAFlat(Room room, Flat flat) {
-		// TODO Auto-generated method stub
-		List<Room> rooms=new ArrayList<Room>();
-		flat.setRooms(rooms);
+	public boolean addRoomToAFlat(Room room, Flat flat) {
+		
+		// if any object is null then we cannot add
+		if(room==null || flat ==null)
+		{
+			return false;
+		}
+		
+		// if the flat contains rooms.
+		if(flat.getRooms()!=null)
+		{
+			flat.getRooms().add(room);
+		}
+		else
+		{
+			// Fat doesn't contain any rooms...
+			flat.setRooms(new CollectionUtility<Room>().addObjectToList(room));
+		}
+		
+		// setting the flat object to the room.
 		room.setFlat(flat);
+		return true;
 	}
 
 	@Override
--- src/com/mani/gayi/pg/app/IFlatAPI.java
+++ src/com/mani/gayi/pg/app/IFlatAPI.java
@@ -2,7 +2,8 @@ package com.mani.gayi.pg.app;
 
 public interface IFlatAPI {
 
-public void addRoomsToAFlat(Room room,Flat flat);
+public boolean addRoomToAFlat(Room room,Flat flat);
+
 public void addFlatToaFloor(Flat flat,Floor floor);
 
 }
