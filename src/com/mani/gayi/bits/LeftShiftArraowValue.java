d240102cbc6a3a42efc74bdde6ebb8fbda01d44c
MGuduri <manidhar.guduri@gmail.com>
2017-03-17 12:22:40 +0530

    Removing unnecessary Bit Problems.

---
+++ src/com/mani/gayi/bits/LeftShiftArraowValue.java
@@ -0,0 +1,11 @@
+package com.mani.gayi.bits;
+
+public class LeftShiftArraowValue {
+	
+	public static void main(String[] args) {
+		long value = 1<<2;
+		System.out.println("Hello value of 1 << 30 : "+value);
+		System.out.println("Integer Max value .."+Integer.MAX_VALUE);
+	}
+
+}
